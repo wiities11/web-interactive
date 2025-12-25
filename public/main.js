a765031fc7de2c8b9b0fe141fc44e875627ba564
wiities11 <supawita_s@cmu.ac.th>
2025-12-25 22:28:05 +0700

    WeatherProject

--- public/main.js
+++ public/main.js
@@ -1,19 +1,32 @@
-const btn = document.querySelector("#btnJoke")
-const setupEl = document.querySelector("#setup")
-const deliveryEl = document.querySelector("#delivery")
-const statusEl = document.querySelector("#status")
-btn.addEventListener("click", async () => {
-statusEl.textContent = "Loading...";
-setupEl.textContent = "-";
-deliveryEl.textContent = "-";
-try {
-const res = await fetch("/api/joke");
-const data = (await res.json())
-setupEl.textContent = data.setup;
-deliveryEl.textContent = data.delivery;
-statusEl.textContent = "";
-} catch (err) {
-console.error(err);
-statusEl.textContent = "Error loading joke.";
-}
+const form = document.querySelector("#weatherForm");
+const cityInput = document.querySelector("#cityInput");
+const statusEl = document.querySelector("#status");
+const cityNameEl = document.querySelector("#cityName");
+const tempEl = document.querySelector("#temp");
+const descEl = document.querySelector("#desc");
+const iconEl = document.querySelector("#icon");
+const aqiEl = document.querySelector("#aqi");
+const pm25El = document.querySelector("#pm25");
+const pm10El = document.querySelector("#pm10");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const city = cityInput.value.trim();
+  statusEl.textContent = "Loading...";
+  try {
+    const res = await fetch(
+      `/api/weather?city=${encodeURIComponent(city || "London")}`
+    );
+    const data = await res.json();
+    cityNameEl.textContent = data.city;
+    tempEl.textContent = String(data.temp);
+    descEl.textContent = data.desc;
+    iconEl.src = data.iconUrl;
+    aqiEl.textContent = String(data.aqi);
+    pm25El.textContent = String(data.pm25);
+    pm10El.textContent = String(data.pm10);
+    statusEl.textContent = "";
+  } catch (err) {
+    console.error(err);
+    statusEl.textContent = "Error fetching weather.";
+  }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,55 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import path from "path";
+
 const app = express();
 const port = 3000;
+
 app.use(express.static(path.join(__dirname, "../public")));
-app.listen(port, () =>
-console.log(`http://localhost:${port}`));
 
+type WeatherResp = {
+    coord: { lat: number; lon: number };
+    main: { temp: number };
+    weather: { description: string }[];
+};
 
-type JokeApiResponse =
-| { type: "twopart"; setup: string; delivery: string }
-| { type: "single"; joke: string };
-app.get("/api/joke", async (req: Request, res: Response) => {
-const url =
-"https://v2.jokeapi.dev/joke/Programming?type=twopart";
-try {
-const apiRes = await fetch(url); // return promise
-const data = (await apiRes.json()) as JokeApiResponse;
-// Guard (TypeScript safety)
-if (data.type !== "twopart") { return
-res.status(502).json({ message: "Unexpected joke format" });
-}
-return res.json({ setup: data.setup, delivery:
-data.delivery });
-} catch (err) {
-console.error(err);
-res.status(500).json({ message: "Failed to fetch joke" });
-}
+type PollutionResp = {
+    list: { main: { aqi: number }; components: { pm2_5: number; pm10: number} }[];
+};
+app.get("/api/weather", async (req: Request, res: Response) => {
+    const city = req.query.city as string;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_KEY}&units=metric`;
+    try {
+        const weatherRes = await fetch(weatherUrl);
+        const weatherData = (await weatherRes.json()) as WeatherResp;
+
+        const { lat, lon } = weatherData.coord;
+        const pollutionUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_KEY}`;
+        const pollutionRes = await fetch(pollutionUrl);
+        const pollutionData = (await pollutionRes.json()) as PollutionResp;
+
+        if (!pollutionData.list?.[0] || !weatherData.weather?.[0]) {
+            return res.status(500).json({ message: "Invalid weather or pollution data" });
+        }
+
+        const aqi = pollutionData.list[0].main.aqi;
+        const pm25 = pollutionData.list[0].components.pm2_5;
+        const pm10 = pollutionData.list[0].components.pm10;
+
+        return res.json({
+            city : city,
+            temp : weatherData.main.temp,
+            desc : weatherData.weather[0].description,
+            iconUrl: "https://openweathermap.org/img/wn/04d@2x.png",
+            aqi : aqi,
+            pm25 : pm25,
+            pm10 : pm10
+        });
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error fetching weather data" });
+    }
 });
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
